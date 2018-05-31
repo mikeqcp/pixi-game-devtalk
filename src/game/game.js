ba82c47a7940e510bd9198b4d5ccf4c97c4bd381
Michal P <michal.przyszczypkowski@gmail.com>
2018-05-31 16:50:47 +0200

    Refactor game instance

---
+++ src/game/game.js
@@ -0,0 +1,66 @@
+import Store from "../helpers/Store";
+import { Plane } from "../characters/Plane";
+import { Enemy } from "../characters/enemy";
+import * as PIXI from "pixi.js";
+import getTexture from "../getTexture";
+import { Application } from "pixi.js";
+import { isEmpty, not } from "ramda";
+import GameState from './game.state';
+
+const gameInstanceOpts = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    antialias: false,
+    transparent: true,
+    autoResize: true,
+    resolution: window.devicePixelRatio,
+};
+
+class Game extends Application {
+    constructor() {
+        super(gameInstanceOpts);
+        this.view.className = "renderArea";
+    }
+
+    setupStage() {
+        document.getElementById("main").appendChild(this.view);
+
+        const bgSprite = new PIXI.Sprite(getTexture('images/background.jpg'));
+        this.stage.addChild(bgSprite);
+
+        this.plane = new Plane();
+
+        this.stage.addChild(this.plane.sprite);
+        this.renderer.render(this.stage);
+
+        this.ticker.add((deltaTime) => {
+            const enemies = Store.get('enemies', []);
+            const bullets = Store.get('bullets', []);
+
+            this.plane.update(deltaTime);
+            enemies.forEach(enemy => enemy.update(deltaTime));
+            bullets.forEach(bullet => bullet.update(deltaTime));
+        });
+
+        this.addEnemies();
+    }
+
+    addEnemies = () => setInterval(() => {
+        const enemy = new Enemy(this.ticker);
+        this.addOrAppendEnemy(enemy);
+
+        this.stage.addChild(enemy.sprite);
+    }, 500);
+
+    addOrAppendEnemy = enemy => {
+        const enemies = Store.get('enemies', []);
+
+        if (not(isEmpty(enemies))) {
+            return Store.add('enemies', [...enemies, enemy]);
+        }
+
+        return Store.add('enemies', [enemy]);
+    }
+}
+
+export default new Game();
--- src/index.js
+++ src/index.js
@@ -1,57 +1,6 @@
-import { Application } from "pixi.js";
-import { not, isEmpty } from 'ramda';
-
-import { Plane } from './characters/Plane';
 import preloadResources from './preloadResources';
-import { Enemy } from './characters/enemy';
-import Store from './helpers/Store';
-import * as PIXI from "pixi.js";
-import getTexture from "./getTexture";
-import GameState from './game/game.state';
-
-class Game extends Application {
-    constructor(app) {
-        super(app);
-        this.view.className = "renderArea";
-        document.getElementById("main").appendChild(this.view);
-
-        const bgSprite = new PIXI.Sprite(getTexture('images/background.jpg'));
-        this.stage.addChild(bgSprite);
-
-        this.plane = new Plane();
-
-        this.stage.addChild(this.plane.sprite);
-        this.renderer.render(this.stage);
-
-        this.ticker.add((deltaTime) => {
-           const enemies = Store.get('enemies', []);
-           const bullets = Store.get('bullets', []);
-
-           this.plane.update(deltaTime);
-           enemies.forEach(enemy => enemy.update(deltaTime));
-           bullets.forEach(bullet => bullet.update(deltaTime));
-        });
-
-        this.addEnemies();
-    }
-
-    addEnemies = () => setInterval(() => {
-        const enemy = new Enemy(this.ticker);
-        this.addOrAppendEnemy(enemy);
-
-        this.stage.addChild(enemy.sprite);
-    }, 500);
-
-    addOrAppendEnemy = enemy => {
-        const enemies = Store.get('enemies', []);
-
-        if (not(isEmpty(enemies))) {
-            return Store.add('enemies', [...enemies, enemy]);
-        }
-
-        return Store.add('enemies', [enemy]);
-    }
-}
+import Game from "./game/game";
+window.game = Game; //todo: deprecate this and use module instead
 
 window.addEventListener("load", () => {
     // List of resources to load
@@ -60,13 +9,6 @@ window.addEventListener("load", () => {
 
     // Then load the images
     preloadResources(resources, () => {
-        window.game = new Game({
-            width: window.innerWidth,
-            height: window.innerHeight,
-            antialias: false,
-            transparent: true,
-            autoResize: true,
-            resolution: window.devicePixelRatio,
-        });
+        Game.setupStage();
     });
 });
