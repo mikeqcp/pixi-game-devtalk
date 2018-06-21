aef93ef77d2b2424a0f1ff87c2375ae0aca3601a
Krzysztof Skoracki <krzysztof@skoracki.net>
2018-06-21 22:57:25 +0200

    Add particles effect to enemies death

--- src/characters/enemy.js
+++ src/characters/enemy.js
@@ -1,9 +1,11 @@
-import { ticker } from 'pixi.js';
+import * as PIXI from 'pixi.js';
+import 'pixi-particles'
 import { getRandomInt } from '../helpers/functions';
 import GameState from '../game/game.state';
 import { createSprite } from "../helpers/sprite";
 import { positionToVector, vectorAsPosition } from "../helpers/vectors";
 import Vector from 'victor';
+import Game from "../game/game";
 
 const SPEED = 3;
 const CATCH_THRESHOLD = 5;
@@ -13,8 +15,101 @@ export class Enemy {
         this._targetEgg = targetEgg;
         this.sprite = createSprite('racoon.png');
         this.sprite.position.set(getRandomInt(window.innerWidth), 0 - this.sprite.height);
-        ticker.shared.add(this.update);
+        PIXI.ticker.shared.add(this.update);
         this._targetPosition = positionToVector(targetEgg.position);
+
+        this.particlesContainer = new PIXI.Container();
+
+        this.emitter = new PIXI.particles.Emitter(
+            this.particlesContainer,
+            // ¯\_(ツ)_/¯
+            ['images/racoon.png'],
+            // Emitter configuration, edit this to change the look
+            // of the emitter
+            // YAY HARDCODING
+            {
+                alpha: {
+                    list: [
+                        {
+                            value: 0.8,
+                            time: 0
+                        },
+                        {
+                            value: 0.1,
+                            time: 1
+                        }
+                    ],
+                    isStepped: false
+                },
+                scale: {
+                    list: [
+                        {
+                            value: 1,
+                            time: 0
+                        },
+                        {
+                            value: 0.3,
+                            time: 1
+                        }
+                    ],
+                    isStepped: false
+                },
+                color: {
+                    list: [
+                        {
+                            value: "fb1010",
+                            time: 0
+                        },
+                        {
+                            value: "f5b830",
+                            time: 1
+                        }
+                    ],
+                    isStepped: false
+                },
+                speed: {
+                    list: [
+                        {
+                            value: 200,
+                            time: 0
+                        },
+                        {
+                            value: 100,
+                            time: 1
+                        }
+                    ],
+                    isStepped: false
+                },
+                startRotation: {
+                    min: 0,
+                    max: 360
+                },
+                rotationSpeed: {
+                    min: 0,
+                    max: 0
+                },
+                lifetime: {
+                    min: 0.5,
+                    max: 0.5
+                },
+                frequency: 0.008,
+                spawnChance: 1,
+                particlesPerWave: 1,
+                emitterLifetime: 0.31,
+                maxParticles: 1000,
+                pos: {
+                    x: 0,
+                    y: 0
+                },
+                addAtBack: false,
+                spawnType: "circle",
+                spawnCircle: {
+                    x: 0,
+                    y: 0,
+                    r: 10
+                }
+            }
+        );
     }
 
     update = () => {
@@ -34,7 +129,17 @@ export class Enemy {
     };
 
     destroy = () => {
-        ticker.shared.remove(this.update);
+
+        // Put the particles container in the middle of the sprite location
+        this.particlesContainer.position.set(
+            this.sprite.position.x + this.sprite.width / 2,
+            this.sprite.position.y + this.sprite.height / 2
+        );
+
+        Game.stage.addChild(this.particlesContainer);
+        this.emitter.playOnceAndDestroy(() => Game.stage.removeChild(this.particlesContainer));
+
+        PIXI.ticker.shared.remove(this.update);
         GameState.enemies.remove(this);
     }
 }
